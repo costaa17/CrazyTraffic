e4f0f75804449e13d8a7e12d4c4d3e2db4e073ea
Ana Vitoria do Valle Costa <CostaA17@anas-macbook-pro.local>
2016-01-08 07:56:39 -0800

    Initial Commit

---
+++ Crazy TrafficTests/Crazy_TrafficTests.swift
@@ -0,0 +1,36 @@
+//
+//  Crazy_TrafficTests.swift
+//  Crazy TrafficTests
+//
+//  Created by Ana Vitoria do Valle Costa on 1/8/16.
+//  Copyright © 2016 Ana Vitoria do Valle Costa. All rights reserved.
+//
+
+import XCTest
+@testable import Crazy_Traffic
+
+class Crazy_TrafficTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
